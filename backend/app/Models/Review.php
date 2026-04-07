391c7903a41bdc8057c71a19ff025849e7a1bba9
Елена <n.a.092024@yandex.ru>
2026-04-07 17:33:26 +0300

    Добавить счетчики отзывов и наблюдателя; синхронизировать счетчики

--- backend/app/Models/Review.php
+++ backend/app/Models/Review.php
@@ -8,8 +8,14 @@ class Review extends Model
 {
     protected $fillable = ['product_id', 'user_id', 'rating', 'comment'];
 
-    public function product() { return $this->belongsTo(Product::class); }
+    public function product() {
+
+        return $this->belongsTo(Product::class);
+    }
+
     public function user() {
+
         return $this->belongsTo(User::class);
     }
+
 }
